223307aeddabf00d0bc517c1d16add27c154fda8
Hyeon Woo Jo <hyeonwoo5342@gmail.com>
2024-05-12 00:15:01 +0900

    chore: Add logs

--- worker/index.js
+++ worker/index.js
@@ -8,12 +8,16 @@ addEventListener("fetch", (event) => {
 async function handleRequest(request) {
   const url = new URL(request.url);
   const pathname = url.pathname.slice(1);
+  console.log("Pathname : ", pathname);
 
   const link = links.find((link) => link.slugs.includes(pathname));
+  console.log("Found Link : ", link);
 
   if (link) {
+    console.log("Redirecting to : ", link.url);
     return Response.redirect(link.url, 301);
   } else {
+    console.log("Link not found");
     return new Response("Link not found", { status: 404 });
   }
 }
